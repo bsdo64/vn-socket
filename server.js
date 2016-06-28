6296e279294509d7489a811e11a1499ef3f22e5a
ByeongSu Do <bsdo@naver.com>
2016-06-29 01:01:43 +0900

    Add noti socket

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-var io = require('socket.io')(3003);
+const cookieParser = require('cookie-parser');
+const Cookie = require('cookie');
+const M = require('vn-api-model');
+
+const io = require('socket.io')(3003);
 
 io.on('connection', function (socket) {
   console.log('socket is opened');
@@ -14,17 +18,37 @@ io.on('connection', function (socket) {
   });
 });
 
-var notiIo = io.of('/noti');
+
+const notiIo = io.of('/noti');
 
 notiIo.on('connection', function (socket) {
   console.log('Noti socket is opened');
 
+  socket.on('send noti', function (result) {
+    
+    notiIo.to(result.to).emit('comment_write noti', result.notis);
+  });
+
+
   socket.on('join_room', function () {
-    console.log('client want to join_room');
+    const headers = socket.request.headers;
+    const cookie = Cookie.parse(headers.cookie);
+    if (cookie.sessionId && cookie.token) {
+      const sessionId = cookieParser.signedCookie(cookie.sessionId, '1234567890QWERTY');
+      const token = cookie.token;
+
+      M
+        .User
+        .checkUserByToken(token, sessionId)
+        .then((user) => {
+          console.log('Join the socket room : ', user.nick);
+          socket.join(user.nick);
+        });
+    }
   });
 
   socket.on('disconnect', function () {
     console.log('user disconnected');
-    //notiIo.emit('user disconnected');
+    socket.leave();
   });
 });
